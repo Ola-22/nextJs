47d6ca1544d2c3617ef81f1b8d3ccb522537dcd5
Ola-22 <olanazly@gmail.com>
2021-03-27 19:16:56 +0300

    Edit Blog Page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=9");
@@ -13,25 +14,27 @@ function blog({ posts }) {
         <h1>blog</h1>
         <hr />
       </div>
+      <div className="row">
+        {
+          posts.map(post => (
+            <div className="col-12 col-md-4" key={post.id}>
+              <div className="card mb-4">
+                <img src={`https://source.unsplash.com/720×400/?${post.id}`} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{post.title}</h5>
+                  <Link href={`/blog/${post.id}`}>
+                    <a className="btn btn-primary">Go somewhere</a>
+                  </Link>
 
-      {
-        posts.map(post => {
-          <div className="col-12 col-md-4" key={post.id}>
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <div className="card" style={{ width: "18rem" }}>
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
               </div>
             </div>
-          </div>
-        })
-      }
+          ))
+        }
+      </div>
     </div>
   )
 }
-
 
 
 export default blog;
